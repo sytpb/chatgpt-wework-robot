f2f658610843b1a928e8dac17980171b6dc91bbb
sytpb <songyantao@foxmail.com>
2023-04-19 12:08:07 +0800

    debug for message response from openai

--- openai.js
+++ openai.js
@@ -2,6 +2,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const models = ['text-davinci-003','code-davinci-002','gpt-3.5-turbo'];
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -10,6 +11,7 @@ const openai = new OpenAIApi(configuration);
 
 const getAIChat = async (question) => {
     
+    console.log(configuration);
     const res = await openai.createChatCompletion({
         model: models[0],
         messages:[{role:"user",content: question}]
